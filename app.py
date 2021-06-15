ffe69d4b6536295924bf72a583f8513d1f6140ae
EC2 Default User <ec2-user@ip-172-31-32-136.ec2.internal>
2021-06-15 14:19:25 +0000

    Initial commit of application and pipeline

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from cdkeks.cdkeks_stack import CdkeksStack
+from cdkeks.pipeline_stack import PipelineStack
+
+app = core.App()
+CdkeksStack(app, "cdkeks")
+PipelineStack(app, 'PipelineStack', env={
+    'account': '128222158613',
+    'region': 'us-west-2',
+})
+app.synth()
